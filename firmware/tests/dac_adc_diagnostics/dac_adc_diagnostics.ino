1c4573d4343ad14e74286f3811a7e8cdf57e37fc
dcfernandez <dfernandez@polysync.io>
2016-12-06 09:32:16 -0800

    Add ADC/DAC diagnostics bench test Prior to this commit, the ADC/DAC diagnostics were not formatted properly. Now, the ADC/DAC diagnostics follow the PS C Style Guide.

--- firmware/tests/dac_adc_diagnostics/dac_adc_diagnostics.ino
+++ firmware/tests/dac_adc_diagnostics/dac_adc_diagnostics.ino
@@ -23,15 +23,15 @@
 
 #define CAN_BAUD ( CAN_500KBPS )
 
-#define SERIAL_BAUD (115200)
+#define SERIAL_BAUD ( 115200 )
 
-#define CAN_INIT_RETRY_DELAY (50)
+#define CAN_INIT_RETRY_DELAY ( 50 )
 
 
-DAC_MCP49xx dac(DAC_MCP49xx::MCP4922, 9); // DAC model, SS pin, LDAC pin
+DAC_MCP49xx dac( DAC_MCP49xx::MCP4922, 9 ); // DAC model, SS pin, LDAC pin
 
 // Construct the CAN shield object
-MCP_CAN CAN(CAN_CS); 
+MCP_CAN CAN( CAN_CS ); 
 
 
 static void init_serial( void ) 
@@ -61,8 +61,8 @@ static void init_can ( void )
 
 // the two DACS have circuitry for measuring the output. Create a signal and
 // measure that the output is what is expected.
-void test_DACS() {
-
+void test_DACS( ) 
+{
     uint32_t dac_value;
     int spoof_signal_a_read = 0;
     int spoof_signal_b_read = 0;
@@ -71,79 +71,90 @@ void test_DACS() {
 
     float dac_expected_output;
 
-    digitalWrite(SPOOF_ENGAGE, HIGH);      // energize the relay so we can read the values at the terminal
+    // energize the relay so we can read the values at the terminal
+    digitalWrite( SPOOF_ENGAGE, HIGH );
 
-
-    for (dac_value = 0; dac_value < 4096; dac_value = dac_value + 15)
+    for ( dac_value = 0; dac_value < 4096; dac_value = dac_value + 15 )
     {
-        dac_expected_output = (5.0/4096.0) * dac_value;
+        dac_expected_output = ( 5.0 / 4096.0 ) * dac_value;
 
-        dac.outputA(dac_value);
-        dac.outputB(dac_value);
+        dac.outputA( dac_value );
+        dac.outputB( dac_value );
 
-        delay(2000);
+        delay( 2000 );
 
-        spoof_signal_a_read = analogRead(SPOOF_SIGNAL_A);
-        spoof_signal_b_read = analogRead(SPOOF_SIGNAL_B);
+        spoof_signal_a_read = analogRead( SPOOF_SIGNAL_A );
+        spoof_signal_b_read = analogRead( SPOOF_SIGNAL_B );
 
-        spoof_a_voltage_read = (spoof_signal_a_read * 5.0 )/ 1024.0;
-        spoof_b_voltage_read = (spoof_signal_b_read * 5.0 )/ 1024.0;
+        spoof_a_voltage_read = ( spoof_signal_a_read * 5.0 ) / 1024.0;
+        spoof_b_voltage_read = ( spoof_signal_b_read * 5.0 ) / 1024.0;
 
-        Serial.print("DAC Value: ");
-        Serial.print(dac_value);
+        Serial.print( "DAC Value: " );
+        Serial.print( dac_value );
 
-        Serial.print("\tExpected Voltage: ");
-        Serial.print(dac_expected_output,3);
+        Serial.print( "\t\t\t\t\tExpected Voltage: " );
+        Serial.println( dac_expected_output,3 );
 
-        Serial.print("\tSpoof A Value: ");
-        Serial.print(spoof_signal_a_read);
+        Serial.print( "Spoof A Value: " );
+        Serial.print( spoof_signal_a_read );
 
-        Serial.print("\tSpoof B Value: ");
-        Serial.print(spoof_signal_b_read);
+        Serial.print( "\tSpoof B Value: " );
+        Serial.print( spoof_signal_b_read );
 
-        Serial.print("\tSpoof A Voltage: ");      
-        Serial.println(spoof_a_voltage_read,3);      
+        Serial.print( "\tSpoof A Voltage: " );      
+        Serial.print( spoof_a_voltage_read, 3 );      
 
-        Serial.print("\tSpoof B Voltage: ");      
-        Serial.println(spoof_b_voltage_read,3);      
+        Serial.print( "\tSpoof B Voltage: " );      
+        Serial.println( spoof_b_voltage_read, 3 );  
+
+        Serial.println( "" );    
     }    
 }
 
 // to test that the signal interrupt relay functions we can blink two
 // LEDS in an alternating pattering using the the switch relay.
-void test_interrupt_relay() {
-
+void test_interrupt_relay( ) 
+{
     Serial.flush(); 
 
     while( !Serial.available() )
     {
-        digitalWrite(SPOOF_ENGAGE, LOW);       // start off with the relay in it's depowered state
-        delay(500);                            // wait half a second
-        digitalWrite(SPOOF_ENGAGE, HIGH);      // energize the relay
-        delay(500);                            // wait half a second
-    }
+        digitalWrite( SPOOF_ENGAGE, LOW );       // start off with the relay in it's depowered state
+        delay( 500 );                            // wait half a second
 
+        digitalWrite( SPOOF_ENGAGE, HIGH );      // energize the relay
+        delay( 500 );                            // wait half a second
+    }
 }
 
 
 // send a CAN frame, to be recieved by some module on a CAN bus.
-void test_CAN_send() {
-
+void test_CAN_send( ) 
+{
     int cantxValue = 60;
 
-    Serial.print("cantxValue: ");
-    Serial.println(cantxValue);
+    Serial.print( "cantxValue: " );
+    Serial.println( cantxValue );
+
     //Create data packet for CAN message
-    unsigned char canMsg[8] = {cantxValue, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    unsigned char canMsg[ 8 ] = {   cantxValue, 
+                                    0x00, 
+                                    0x00, 
+                                    0x00, 
+                                    0x00, 
+                                    0x00, 
+                                    0x00, 
+                                    0x00 };
+
     // send data:  id = 0x123, standrad frame, data len = 8, stmp: data buf
-    CAN.sendMsgBuf(0x07B, 0, 8, canMsg); 
-    delay(250);
+    CAN.sendMsgBuf( 0x07B, 0, 8, canMsg ); 
+    delay( 250 );
 }
 
 
 // recieve a CAN frame sent from some module on a CAN bus.
-void test_CAN_recieve() {
-
+void test_CAN_recieve( ) 
+{
     // local vars
     can_frame_s rx_frame;
 
@@ -156,62 +167,64 @@ void test_CAN_recieve() {
                 (INT32U*) &rx_frame.id,
                 (INT8U*) &rx_frame.dlc,
                 (INT8U*) rx_frame.data );
-        Serial.print("canRxValue:");
-        Serial.println(rx_frame.id);
+
+        Serial.print( "canRxValue:" );
+        Serial.println( rx_frame.id );
         delay(250);
     }
-
 }
 
 //INSTALLATION TESTS
 
-
-void test_signal_sense() {
-    // turn steering wheel while the car is on and check that the readings from
-    // the torque sensors are as expected.
-
+// turn steering wheel while the car is on and check that the readings from
+// the torque sensors are as expected.      
+void test_signal_sense( ) 
+{
     int signal_a_read = 0;
     int signal_b_read = 0;
     float a_voltage_read = 0;
     float b_voltage_read = 0;
 
-    signal_a_read = analogRead(SIGNAL_INPUT_A);
-    signal_b_read = analogRead(SIGNAL_INPUT_B);
+    signal_a_read = analogRead( SIGNAL_INPUT_A );
+    signal_b_read = analogRead( SIGNAL_INPUT_B );
 
-    a_voltage_read = (signal_a_read * 5.0 )/ 1024.0;
-    b_voltage_read = (signal_b_read * 5.0 )/ 1024.0;
+    a_voltage_read = ( signal_a_read * 5.0 ) / 1024.0;
+    b_voltage_read = ( signal_b_read * 5.0 ) / 1024.0;
 
-    Serial.print("Signal A Value: ");
-    Serial.print(a_voltage_read);
+    Serial.print( "Signal A Value: " );
+    Serial.print( a_voltage_read );
 
-    Serial.print("\tSignal B Value: ");
-    Serial.println(b_voltage_read);
+    Serial.print( "\tSignal B Value: " );
+    Serial.println( b_voltage_read );
 }
 
 
-void test_torque_spoof() {
+void test_torque_spoof( ) 
+{
     // send spoofed torque, slowly sweep stering back and forth.
 }
 
 
-void test_hard_power_off() {
+void test_hard_power_off( ) 
+{
     // while sweeping the steering wheel, cut power and test that shutdown is
     // handled gracefully or throws the expected fault.
 }
 
 
-void setup() {
+void setup( ) 
+{
     init_serial();
     init_can();
 }
 
 
-void loop() {
-
-    //test_DACS();
+void loop() 
+{
+    test_DACS();
     //test_interrupt_relay();
     //test_CAN_send();
     //test_CAN_recieve();
-    test_signal_sense();
+    //test_signal_sense();
 
 }
