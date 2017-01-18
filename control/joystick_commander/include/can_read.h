05613c7f1a1715bc58f505e672ffb4fdc5c63106
Lucas Buckland <lbuckland@polysync.io>
2017-01-17 18:39:40 -0800

    Add joystick commander support for driver override Prior to this commit the joystick commander was not checking report messages from the steering, throttle and brake modules to see if there had been a driver override. This commit adds the functionality to check incoming CAN report messages for the driver override flag. This commit also includes the code to properly disable all control modules if a report message with driver override is received.

--- control/joystick_commander/include/can_read.h
+++ control/joystick_commander/include/can_read.h
@@ -27,59 +27,31 @@
 /************************************************************************/
 
 /**
- * @file messages.h
- * @brief Message Utilities Interface.
+ * @file can_read.h
+ * @brief Can read interface.
  *
  */
 
 
 
 
-#ifndef MESSAGES_H
-#define MESSAGES_H
+#ifndef CAN_READ_H
+#define CAN_READ_H
 
 
 
 
-#include "control_protocol_can.h"
-
-
-
-
-/**
- * @brief Message set.
- *
- * Container for PolySync messages.
- *
- */
-
-
-typedef struct
-{
-    //
-    //
-    ps_ctrl_brake_command_msg brake_cmd; /*!< Platform brake command message. */
-    //
-    //
-    ps_ctrl_throttle_command_msg throttle_cmd; /*!< Platform throttle command message. */
-    //
-    //
-    ps_ctrl_steering_command_msg steering_cmd; /*!< Platform steering wheel command message. */
-} messages_s;
+#include "commander.h"
 
 
 
 
 //
-int messages_is_valid(
-        const messages_s * const const messages );
-
-
-//
-int messages_set_default_values(
-        messages_s * const messages );
+int can_read_buffer(
+        commander_s * const commander );
 
 
 
 
-#endif	/* MESSAGES_H */
+#endif /* CAN_READ_H */
+
