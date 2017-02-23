4e16e2286165a0ec1d003d7be3978c981a0de1c3
Lucas Buckland <lbuckland@polysync.io>
2017-02-22 16:28:25 -0800

    Add terminal print to diagnostics tool Prior to this commit the diagnostics tool did not include the functionality to properly print to the command line. This commit adds the source files necessary to the diagnostics tool to allow it to properly print to the command line.

---
+++ control/diagnostics_tool/src/terminal_print.c
@@ -0,0 +1,117 @@
+/************************************************************************/
+/* The MIT License (MIT)                                                */
+/* =====================                                                */
+/*                                                                      */
+/* Copyright (c) 2017 PolySync Technologies, Inc.  All Rights Reserved. */
+/*                                                                      */
+/* Permission is hereby granted, free of charge, to any person          */
+/* obtaining a copy of this software and associated documentation       */
+/* files (the “Software”), to deal in the Software without              */
+/* restriction, including without limitation the rights to use,         */
+/* copy, modify, merge, publish, distribute, sublicense, and/or sell    */
+/* copies of the Software, and to permit persons to whom the            */
+/* Software is furnished to do so, subject to the following             */
+/* conditions:                                                          */
+/*                                                                      */
+/* The above copyright notice and this permission notice shall be       */
+/* included in all copies or substantial portions of the Software.      */
+/*                                                                      */
+/* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,      */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      */
+/* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND             */
+/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT          */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,         */
+/* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING         */
+/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        */
+/* OTHER DEALINGS IN THE SOFTWARE.                                      */
+/************************************************************************/
+
+
+
+
+/**
+ * @file terminal_print.c
+ * @brief Debug printer functions.
+ *
+ */
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+#include "terminal_print.h"
+
+
+
+
+// *****************************************************
+// static global veriables
+// *****************************************************
+
+
+static char lines[ MAX_LINES ][ LINE_SIZE ];
+
+
+static int num_lines = 0;
+
+
+static int last_lines = 0;
+
+
+
+
+// *****************************************************
+// static declarations
+// *****************************************************
+
+
+//
+static void printNewLine( const char * name )
+{
+    printf( "%s\n", name );
+}
+
+
+
+
+// *****************************************************
+// public definitions
+// *****************************************************
+
+
+//
+void addLine( char * line )
+{
+    strcpy( lines[ num_lines ], line );
+
+    num_lines++;
+}
+
+
+//
+void printLines()
+{
+    int i;
+
+    for( i = 0; i < last_lines; i++ )
+    {
+        fputs( "\033[A\033[2K", stdout );
+
+        rewind( stdout );
+
+        ftruncate( 1, 0 );
+    }
+
+    last_lines = 0;
+
+    for( i = 0; i < num_lines; i++ )
+    {
+        printNewLine( lines[ i ] );
+
+        last_lines++;
+    }
+
+    num_lines = 0;
+}
