61afecdc4e72accec264575348883ee22ad1cbd7
Lucas Buckland <lbuckland@polysync.io>
2017-03-08 17:54:46 -0800

    Add new copyright and cleaner error checking Prior to this commit there was an oudated copyright in this file. In addition some of the error checking done was redundant and not as clean as it could be. This commit adds the new copyright and cleans up some of the null pointer and error checking.

--- control/diagnostics_tool/src/system_state.c
+++ control/diagnostics_tool/src/system_state.c
@@ -1,29 +1,29 @@
 /************************************************************************/
-/* The MIT License (MIT) */
-/* ===================== */
-
-/* Copyright (c) 2016 PolySync Technologies, Inc.  All Rights Reserved. */
-
-/* Permission is hereby granted, free of charge, to any person */
-/* obtaining a copy of this software and associated documentation */
-/* files (the “Software”), to deal in the Software without */
-/* restriction, including without limitation the rights to use, */
-/* copy, modify, merge, publish, distribute, sublicense, and/or sell */
-/* copies of the Software, and to permit persons to whom the */
-/* Software is furnished to do so, subject to the following */
-/* conditions: */
-
-/* The above copyright notice and this permission notice shall be */
-/* included in all copies or substantial portions of the Software. */
-
-/* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES */
-/* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND */
-/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT */
-/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, */
-/* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING */
-/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR */
-/* OTHER DEALINGS IN THE SOFTWARE. */
+/* The MIT License (MIT)                                                */
+/* =====================                                                */
+/*                                                                      */
+/* Copyright (c) 2017 PolySync Technologies, Inc.  All Rights Reserved. */
+/*                                                                      */
+/* Permission is hereby granted, free of charge, to any person          */
+/* obtaining a copy of this software and associated documentation       */
+/* files (the “Software”), to deal in the Software without              */
+/* restriction, including without limitation the rights to use,         */
+/* copy, modify, merge, publish, distribute, sublicense, and/or sell    */
+/* copies of the Software, and to permit persons to whom the            */
+/* Software is furnished to do so, subject to the following             */
+/* conditions:                                                          */
+/*                                                                      */
+/* The above copyright notice and this permission notice shall be       */
+/* included in all copies or substantial portions of the Software.      */
+/*                                                                      */
+/* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,      */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      */
+/* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND             */
+/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT          */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,         */
+/* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING         */
+/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        */
+/* OTHER DEALINGS IN THE SOFTWARE.                                      */
 /************************************************************************/
 
 
@@ -65,7 +65,7 @@ static current_system_state_s system_state;
 //
 static int update_steering_state()
 {
-    int ret = NOERR;
+    int ret = ERROR;
 
     const can_frame_s * const steering_command_frame =
             get_can_msg_array_index_reference( PS_CTRL_MSG_ID_STEERING_COMMAND );
@@ -73,19 +73,14 @@ static int update_steering_state()
     const can_frame_s * const steering_report_frame =
             get_can_msg_array_index_reference( PS_CTRL_MSG_ID_STEERING_REPORT );
 
-    if( steering_command_frame == NULL  ||  steering_report_frame == NULL )
-    {
-        ret = ERROR;
-    }
-
-    if( ret == NOERR )
+    if( steering_command_frame != NULL  ||  steering_report_frame != NULL )
     {
         ret = analyze_steering_state(
                 &system_state.steering_module_state,
                 steering_command_frame,
                 steering_report_frame );
     }
-
+    
     return ret;
 }
 
@@ -93,7 +88,7 @@ static int update_steering_state()
 //
 static int update_throttle_state()
 {
-    int ret = NOERR;
+    int ret = ERROR;
 
     const can_frame_s * const throttle_command_frame =
             get_can_msg_array_index_reference( PS_CTRL_THROTTLE_COMMAND_ID );
@@ -101,12 +96,7 @@ static int update_throttle_state()
     const can_frame_s * const throttle_report_frame =
             get_can_msg_array_index_reference( PS_CTRL_MSG_ID_THROTTLE_REPORT );
 
-    if( throttle_command_frame == NULL || throttle_report_frame == NULL )
-    {
-        ret = ERROR;
-    }
-
-    if( ret == NOERR )
+    if( throttle_command_frame != NULL || throttle_report_frame != NULL )
     {
         ret = analyze_throttle_state(
                 &system_state.throttle_module_state,
@@ -121,7 +111,7 @@ static int update_throttle_state()
 //
 static int update_brake_state()
 {
-    int ret = NOERR;
+    int ret = ERROR;
 
     const can_frame_s * const brake_command_frame =
             get_can_msg_array_index_reference( PS_CTRL_MSG_ID_BRAKE_COMMAND );
@@ -129,12 +119,7 @@ static int update_brake_state()
     const can_frame_s * const brake_report_frame =
             get_can_msg_array_index_reference( PS_CTRL_MSG_ID_BRAKE_REPORT );
 
-    if( brake_command_frame == NULL  ||  brake_report_frame == NULL )
-    {
-        ret = ERROR;
-    }
-
-    if( ret == NOERR )
+    if( brake_command_frame != NULL  ||  brake_report_frame != NULL )
     {
         ret = analyze_brake_state(
                 &system_state.brake_module_state,
@@ -149,7 +134,7 @@ static int update_brake_state()
 //
 static int update_gateway_state()
 {
-    int ret = NOERR;
+    int ret = ERROR;
 
     const can_frame_s * const heartbeat_msg_frame =
             get_can_msg_array_index_reference( PSVC_HEARTBEAT_MSG_BASE_ID );
@@ -160,14 +145,9 @@ static int update_gateway_state()
     const can_frame_s * const chassis_state2_frame =
             get_can_msg_array_index_reference( PSVC_CHASSIS_STATE2_MSG_ID );
 
-    if( heartbeat_msg_frame == NULL ||
-            chassis_state1_frame == NULL ||
-            chassis_state2_frame == NULL )
-    {
-        ret = ERROR;
-    }
-
-    if( ret == NOERR )
+    if( heartbeat_msg_frame != NULL ||
+            chassis_state1_frame != NULL ||
+            chassis_state2_frame != NULL )
     {
         ret = analyze_gateway_state(
                 &system_state.gateway_module_state,
