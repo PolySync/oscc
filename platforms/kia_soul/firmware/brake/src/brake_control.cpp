4a313a08347e357d0f1a2c011318e54e40d378f6
Austin Morlan <amorlan@polysync.io>
2017-06-01 12:08:38 -0700

    Use signal smoothing library in brake module
    Prior to this commit, the brake module was using an exponential
    moving average on the values read from its master cylinder pressure
    sensor, but it was taking place in the operator override section.
    This commit creates a function for reading the master cylinder pressure
    and moves the filtering into it. It also uses the new signal smoothing
    library function.

--- platforms/kia_soul/firmware/brake/src/brake_control.cpp
+++ platforms/kia_soul/firmware/brake/src/brake_control.cpp
@@ -78,43 +78,12 @@ void check_for_operator_override( void )
     if( g_brake_control_state.enabled == true
         || g_brake_control_state.operator_override == true )
     {
-        // This function checks the voltage input from the brake pedal
-        // sensors to determine if the driver is attempting to brake
-        // the vehicle.  This must be done over time by taking
-        // periodic samples of the input voltage, calculating the
-        // difference between the two and then passing that difference
-        // through a basic exponential filter to smooth the input.
+        master_cylinder_pressure_s master_cylinder_pressure;
 
-        // The required response time for the filter is 250 ms, which at
-        // 50ms per sample is 5 samples.  As such, the alpha for the
-        // exponential filter is 0.5 to make the input go "close to" zero
-        // in 5 samples.
+        master_cylinder_read_pressure( &master_cylinder_pressure );
 
-        // The implementation is:
-        //     s(t) = ( a * x(t) ) + ( ( 1 - a ) * s ( t - 1 ) )
-
-        // If the filtered input exceeds the max voltage, it is an
-        // indicator that the driver is pressing on the brake pedal
-        // and the control should be disabled.
-        static const float filter_alpha = 0.05;
-
-        static float filtered_input_1 = 0.0;
-        static float filtered_input_2 = 0.0;
-
-        float sensor_1 = ( float )( analogRead( PIN_MASTER_CYLINDER_PRESSURE_SENSOR_1 ) );
-        float sensor_2 = ( float )( analogRead( PIN_MASTER_CYLINDER_PRESSURE_SENSOR_2 ) );
-
-        sensor_1 = raw_adc_to_pressure( ( uint16_t )sensor_1 );
-        sensor_2 = raw_adc_to_pressure( ( uint16_t )sensor_2 );
-
-        filtered_input_1 = ( filter_alpha * sensor_1 ) +
-            ( ( 1.0 - filter_alpha ) * filtered_input_1 );
-
-        filtered_input_2 = ( filter_alpha * sensor_2 ) +
-            ( ( 1.0 - filter_alpha ) * filtered_input_2 );
-
-        if ( ( filtered_input_1 >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS ) ||
-            ( filtered_input_2 >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS ) )
+        if ( ( master_cylinder_pressure.sensor_1_pressure >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS ) ||
+            ( master_cylinder_pressure.sensor_2_pressure >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS ) )
         {
             disable_control( );
 
