ad0fdf64a8124963c04791a4cbe0db5ff0379bb8
Austin Morlan <amorlan@polysync.io>
2017-07-24 15:51:54 -0700

    Update steering tests

---
+++ firmware/kia_soul/steering/tests/features/checking_faults.feature
@@ -0,0 +1,47 @@
+# language: en
+
+Feature: Checking for fauls
+
+  If the module encounters a fault condition, it should disable control and
+  publish a fault report.
+
+  Scenario: A sensor becomes temporarily disconnected
+    Given steering control is enabled
+
+    When a sensor becomes temporarily disconnected
+
+    Then control should remain enabled
+
+
+  Scenario: A sensor becomes permanently disconnected
+    Given steering control is enabled
+
+    When a sensor becomes permanently disconnected
+
+    Then control should be disabled
+    And a fault report should be published
+
+
+  Scenario: Controller command timeout
+    Given steering control is enabled
+
+    When the time since the last received controller command exceeds the timeout
+
+    Then control should be disabled
+    And a fault report should be published
+
+
+  Scenario Outline: Operator override
+    Given steering control is enabled
+
+    When the operator applies <sensor_val> to the steering wheel
+
+    Then control should be disabled
+    And a fault report should be published
+
+    Examples:
+      | sensor_val |
+      |  800       |
+      |  1024      |
+      |  2048      |
+      |  4096      |
--- firmware/kia_soul/steering/tests/features/step_definitions/receiving_messages.cpp
+++ firmware/kia_soul/steering/tests/features/step_definitions/receiving_messages.cpp
@@ -1,5 +1,8 @@
 WHEN("^an enable steering command is received$")
 {
+    g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL;
+    g_mock_mcp_can_read_msg_buf_id = OSCC_STEERING_COMMAND_CAN_ID;
+
     oscc_steering_command_s * steering_command =
         (oscc_steering_command_s *) g_mock_mcp_can_read_msg_buf_buf;
 
@@ -11,6 +14,9 @@ WHEN("^an enable steering command is received$")
 
 WHEN("^a disable steering command is received$")
 {
+    g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL;
+    g_mock_mcp_can_read_msg_buf_id = OSCC_STEERING_COMMAND_CAN_ID;
+
     oscc_steering_command_s * steering_command =
         (oscc_steering_command_s *) g_mock_mcp_can_read_msg_buf_buf;
 
@@ -20,10 +26,23 @@ WHEN("^a disable steering command is received$")
 }
 
 
+WHEN("^a fault report is received$")
+{
+    g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL;
+    g_mock_mcp_can_read_msg_buf_id = OSCC_FAULT_REPORT_CAN_ID;
+
+    check_for_incoming_message();
+}
+
+
 WHEN("^a command is received with spoof values (.*) and (.*)$")
 {
     REGEX_PARAM(uint16_t, high);
     REGEX_PARAM(uint16_t, low);
+
+    g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL;
+    g_mock_mcp_can_read_msg_buf_id = OSCC_STEERING_COMMAND_CAN_ID;
+
 
     oscc_steering_command_s * steering_command =
         (oscc_steering_command_s *) g_mock_mcp_can_read_msg_buf_buf;
