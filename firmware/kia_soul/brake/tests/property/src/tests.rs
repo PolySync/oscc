059a70b2f9f9b148c128713fcb83ecaabe53adea
Katie Cleary <kcleary@polysync.io>
2017-07-27 14:41:54 -0700

    Start updating brake PBT
    Prior to this commit, all of the brake property based tests were out of date. This commit starts to refactor them to work with the new firmware organization.

--- firmware/kia_soul/brake/tests/property/src/tests.rs
+++ firmware/kia_soul/brake/tests/property/src/tests.rs
@@ -10,72 +10,55 @@ extern crate quickcheck;
 extern crate rand;
 
 use quickcheck::{QuickCheck, TestResult, Arbitrary, Gen, StdGen};
+use rand::Rng;
 
 extern "C" {
     #[link_name = "g_mock_mcp_can_check_receive_return"]
     pub static mut g_mock_mcp_can_check_receive_return: u8;
     #[link_name = "g_mock_mcp_can_read_msg_buf_id"]
-    pub static mut g_mock_mcp_can_read_msg_buf_id: ::std::os::raw::c_ulong;
+    pub static mut g_mock_mcp_can_read_msg_buf_id: u32;
     #[link_name = "g_mock_mcp_can_read_msg_buf_buf"]
     pub static mut g_mock_mcp_can_read_msg_buf_buf: [u8; 8usize];
     #[link_name = "g_mock_mcp_can_send_msg_buf_id"]
-    pub static mut g_mock_mcp_can_send_msg_buf_id: ::std::os::raw::c_ulong;
+    pub static mut g_mock_mcp_can_send_msg_buf_id: u32;
     #[link_name = "g_mock_mcp_can_send_msg_buf_ext"]
     pub static mut g_mock_mcp_can_send_msg_buf_ext: u8;
     #[link_name = "g_mock_mcp_can_send_msg_buf_len"]
     pub static mut g_mock_mcp_can_send_msg_buf_len: u8;
     #[link_name = "g_mock_mcp_can_send_msg_buf_buf"]
     pub static mut g_mock_mcp_can_send_msg_buf_buf: *mut u8;
-    #[link_name = "g_mock_arduino_millis_return"]
-    pub static mut g_mock_arduino_millis_return: ::std::os::raw::c_ulong;
     #[link_name = "g_mock_arduino_analog_read_return"]
-    pub static mut g_mock_arduino_analog_read_return: u16;
+    pub static mut g_mock_arduino_analog_read_return: isize;
+    #[link_name = "g_mock_dac_output_a"]
+    pub static mut g_mock_dac_output_a: u16;
+    #[link_name = "g_mock_dac_output_b"]
+    pub static mut g_mock_dac_output_b: u16;
+    #[link_name = "g_brake_control_state"]
+    pub static mut g_brake_control_state: kia_soul_brake_control_state_s;
 }
 
-impl Arbitrary for oscc_report_brake_data_s {
-    fn arbitrary<G: Gen>(g: &mut G) -> oscc_report_brake_data_s {
-        oscc_report_brake_data_s {
-            pedal_input: i16::arbitrary(g),
+impl Arbitrary for oscc_brake_report_s {
+    fn arbitrary<G: Gen>(g: &mut G) -> oscc_brake_report_s {
+        oscc_brake_report_s {
+            magic: [OSCC_MAGIC_BYTE_0 as u8, OSCC_MAGIC_BYTE_1 as u8],
+            enabled: u8::arbitrary(g),
+            operator_override: u8::arbitrary(g),
+            dtcs: u8::arbitrary(g),
+            reserved: [u8::arbitrary(g); 3]
+        }
+    }
+}
+
+impl Arbitrary for oscc_brake_command_s {
+    fn arbitrary<G: Gen>(g: &mut G) -> oscc_brake_command_s {
+        oscc_brake_command_s {
+            magic: [OSCC_MAGIC_BYTE_0 as u8, OSCC_MAGIC_BYTE_1 as u8],
             pedal_command: u16::arbitrary(g),
-            pedal_output: u16::arbitrary(g),
-            _bitfield_1: u16::arbitrary(g),
+            enable: u8::arbitrary(g),
+            reserved: [u8::arbitrary(g); 3]
         }
     }
 }
-
-impl Arbitrary for oscc_report_brake_s {
-    fn arbitrary<G: Gen>(g: &mut G) -> oscc_report_brake_s {
-        oscc_report_brake_s {
-            id: u32::arbitrary(g),
-            dlc: u8::arbitrary(g),
-            timestamp: u32::arbitrary(g),
-            data: oscc_report_brake_data_s::arbitrary(g),
-        }
-    }
-}
-
-impl Arbitrary for oscc_command_brake_data_s {
-    fn arbitrary<G: Gen>(g: &mut G) -> oscc_command_brake_data_s {
-        oscc_command_brake_data_s {
-            pedal_command: u16::arbitrary(g),
-            _bitfield_1: u16::arbitrary(g),
-            reserved_5: u8::arbitrary(g),
-            reserved_6: u8::arbitrary(g),
-            reserved_7: u8::arbitrary(g),
-            reserved_8: u8::arbitrary(g),
-        }
-    }
-}
-
-impl Arbitrary for oscc_command_brake_s {
-    fn arbitrary<G: Gen>(g: &mut G) -> oscc_command_brake_s {
-        oscc_command_brake_s {
-            timestamp: u32::arbitrary(g),
-            data: oscc_command_brake_data_s::arbitrary(g),
-        }
-    }
-}
-
 
 impl Arbitrary for can_frame_s {
     fn arbitrary<G: Gen>(g: &mut G) -> can_frame_s {
@@ -83,35 +66,36 @@ impl Arbitrary for can_frame_s {
             id: u32::arbitrary(g),
             dlc: u8::arbitrary(g),
             timestamp: u32::arbitrary(g),
-            data: [u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g),
-                   u8::arbitrary(g)],
+            data: [u8::arbitrary(g); 8]
         }
     }
 }
 
-/// the throttle firmware should not attempt processing any messages
-/// that are not throttle commands
-fn prop_only_process_valid_messages(rx_can_msg: can_frame_s, current_target: f32) -> TestResult {
-    // if we generate a throttle can message, ignore the result
-    if rx_can_msg.id == OSCC_COMMAND_BRAKE_CAN_ID {
+/// the brake firmware should not attempt processing any messages
+/// that are not brake or fault commands
+/// this means that none of the brake control state would
+/// change.
+fn prop_only_process_valid_messages(rx_can_msg: can_frame_s, enabled: bool, operator_override: bool, dtcs: u8) -> TestResult {
+    if rx_can_msg.id == OSCC_BRAKE_COMMAND_CAN_ID || 
+       rx_can_msg.id == OSCC_FAULT_REPORT_CAN_ID
+    {
         return TestResult::discard();
     }
     unsafe {
-        g_brake_control_state.commanded_pedal_position = current_target;
+        g_brake_control_state.enabled = enabled;
+        g_brake_control_state.operator_override = operator_override;
+        g_brake_control_state.dtcs = dtcs;
 
-        g_mock_mcp_can_read_msg_buf_id = rx_can_msg.id as u64;
+        g_mock_mcp_can_read_msg_buf_id = rx_can_msg.id;
         g_mock_mcp_can_read_msg_buf_buf = rx_can_msg.data;
         g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
 
         check_for_incoming_message();
 
-        TestResult::from_bool(g_brake_control_state.commanded_pedal_position == current_target)
+        TestResult::from_bool(g_brake_control_state.enabled ==
+                              enabled &&
+                              g_brake_control_state.operator_override == operator_override &&
+                              g_brake_control_state.dtcs == dtcs)
     }
 }
 
@@ -119,43 +103,22 @@ fn prop_only_process_valid_messages(rx_can_msg: can_frame_s, current_target: f32
 fn check_message_type_validity() {
     QuickCheck::new()
         .tests(1000)
-        .gen(StdGen::new(rand::thread_rng(), u16::max_value() as usize))
-        .quickcheck(prop_only_process_valid_messages as fn(can_frame_s, f32) -> TestResult)
+        .gen(StdGen::new(rand::thread_rng(), u32::max_value() as usize))
+        .quickcheck(prop_only_process_valid_messages as fn(can_frame_s, bool, bool, u8) -> TestResult)
 }
 
-/// the throttle firmware should set the commanded pedal position
-/// upon reciept of a valid command throttle message
-fn prop_no_invalid_targets(command_brake_msg: oscc_command_brake_s) -> TestResult {
+/// the brake firmware should set the control state as enabled
+/// upon reciept of a valid command brake message telling it to enable
+fn prop_process_enable_command(mut brake_command_msg: oscc_brake_command_s) -> TestResult {
     unsafe {
-        g_mock_mcp_can_read_msg_buf_id = OSCC_COMMAND_BRAKE_CAN_ID as u64;
-        g_mock_mcp_can_read_msg_buf_buf = std::mem::transmute(command_brake_msg.data);
-        g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
+        brake_command_msg.enable = 1u8;
 
-        check_for_incoming_message();
-
-        TestResult::from_bool(g_brake_control_state.commanded_pedal_position ==
-                              command_brake_msg.data.pedal_command as f32)
-    }
-}
-
-#[test]
-fn check_accel_pos_validity() {
-    QuickCheck::new()
-        .tests(1000)
-        .quickcheck(prop_no_invalid_targets as fn(oscc_command_brake_s) -> TestResult)
-}
-
-/// the throttle firmware should set the control state as enabled
-/// upon reciept of a valid command throttle message telling it to enable
-fn prop_process_enable_command(mut command_brake_msg: oscc_command_brake_s) -> TestResult {
-    unsafe {
-        command_brake_msg.data.set_enabled(1);
-        
+        g_brake_control_state.enabled = false;
         g_brake_control_state.operator_override = false;
 
-        g_mock_mcp_can_read_msg_buf_id = OSCC_COMMAND_BRAKE_CAN_ID as u64;
-        g_mock_mcp_can_read_msg_buf_buf = std::mem::transmute(command_brake_msg.data);
+        g_mock_mcp_can_read_msg_buf_id = OSCC_BRAKE_COMMAND_CAN_ID;
         g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
+        g_mock_mcp_can_read_msg_buf_buf = brake_command_msg as [u8; 8];
 
         check_for_incoming_message();
 
@@ -164,20 +127,21 @@ fn prop_process_enable_command(mut command_brake_msg: oscc_command_brake_s) -> T
 }
 
 #[test]
+// #[ignore]
 fn check_process_enable_command() {
     QuickCheck::new()
         .tests(1000)
-        .quickcheck(prop_process_enable_command as fn(oscc_command_brake_s) -> TestResult)
+        .quickcheck(prop_process_enable_command as fn(oscc_brake_command_s) -> TestResult)
 }
 
-/// the throttle firmware should set the control state as disabled
-/// upon reciept of a valid command throttle message telling it to disable
-fn prop_process_disable_command(mut command_brake_msg: oscc_command_brake_s) -> TestResult {
+/// the brake firmware should set the control state as disabled
+/// upon reciept of a valid command brake message telling it to disable
+fn prop_process_disable_command(mut brake_command_msg: oscc_brake_command_s) -> TestResult {
     unsafe {
-        command_brake_msg.data.set_enabled(0);
+        brake_command_msg.enable = 0u8;
 
-        g_mock_mcp_can_read_msg_buf_id = OSCC_COMMAND_BRAKE_CAN_ID as u64;
-        g_mock_mcp_can_read_msg_buf_buf = std::mem::transmute(command_brake_msg.data);
+        g_mock_mcp_can_read_msg_buf_id = OSCC_BRAKE_COMMAND_CAN_ID;
+        g_mock_mcp_can_read_msg_buf_buf = brake_command_msg as [u8; 8];
         g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
 
         check_for_incoming_message();
@@ -187,81 +151,114 @@ fn prop_process_disable_command(mut command_brake_msg: oscc_command_brake_s) -> 
 }
 
 #[test]
+#[ignore]
 fn check_process_disable_command() {
     QuickCheck::new()
         .tests(1000)
-        .quickcheck(prop_process_disable_command as fn(oscc_command_brake_s) -> TestResult)
+        .quickcheck(prop_process_disable_command as fn(oscc_brake_command_s) -> TestResult)
 }
 
-/// the throttle firmware should create only valid CAN frames
-fn prop_send_valid_can_fields(control_enabled: bool,
-                              operator_override: bool,
-                              commanded_pedal_position: f32)
-                              -> TestResult {
-    static mut time: u64 = 0;
+/// the brake firmware should send requested spoof values
+/// upon recieving a brake command message
+fn prop_output_accurate_spoofs(mut brake_command_msg: oscc_brake_command_s) -> TestResult {
+    brake_command_msg.enable = 1u8;
+    brake_command_msg.pedal_command = rand::thread_rng().gen_range(MINIMUM_BRAKE_COMMAND as u16, MAXIMUM_BRAKE_COMMAND as u16);
     unsafe {
-        g_brake_control_state.operator_override = operator_override;
-        g_brake_control_state.commanded_pedal_position = commanded_pedal_position;
+        g_brake_control_state.enabled = true;
 
-        time = time + OSCC_REPORT_BRAKE_PUBLISH_INTERVAL_IN_MSEC as u64;
+        g_mock_mcp_can_read_msg_buf_id = OSCC_BRAKE_COMMAND_CAN_ID;
+        g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
+        g_mock_mcp_can_read_msg_buf_buf = std::mem::transmute(brake_command_msg);
 
-        g_mock_arduino_millis_return = time;
+        check_for_incoming_message();
 
-        publish_reports();
-
-        let brake_data = oscc_report_brake_data_s {
-            pedal_input: std::mem::transmute([*g_mock_mcp_can_send_msg_buf_buf,
-                                              *g_mock_mcp_can_send_msg_buf_buf.offset(1)]),
-            pedal_command: std::mem::transmute([*g_mock_mcp_can_send_msg_buf_buf.offset(2),
-                                                *g_mock_mcp_can_send_msg_buf_buf.offset(3)]),
-            pedal_output: std::mem::transmute([*g_mock_mcp_can_send_msg_buf_buf.offset(4),
-                                               *g_mock_mcp_can_send_msg_buf_buf.offset(5)]),
-            _bitfield_1: std::mem::transmute([*g_mock_mcp_can_send_msg_buf_buf.offset(6),
-                                              *g_mock_mcp_can_send_msg_buf_buf.offset(7)]),
-        };
-
-        TestResult::from_bool((g_mock_mcp_can_send_msg_buf_id == OSCC_REPORT_BRAKE_CAN_ID as u64) &&
-                              (g_mock_mcp_can_send_msg_buf_ext == (CAN_STANDARD as u8)) &&
-                              (g_mock_mcp_can_send_msg_buf_len ==
-                               (OSCC_REPORT_BRAKE_CAN_DLC as u8)) &&
-                              (brake_data.pedal_command == commanded_pedal_position as u16) &&
-                              (brake_data.enabled() == g_brake_control_state.enabled as u8) &&
-                              (brake_data.override_() == operator_override as u8))
+        TestResult::from_bool(g_brake_control_state.enabled)
     }
 }
 
 #[test]
-fn check_valid_can_frame() {
+fn check_output_accurate_spoofs() {
     QuickCheck::new()
         .tests(1000)
         .gen(StdGen::new(rand::thread_rng(), u16::max_value() as usize))
-        .quickcheck(prop_send_valid_can_fields as fn(bool, bool, f32) -> TestResult)
+        .quickcheck(prop_output_accurate_spoofs as fn(oscc_brake_command_s) -> TestResult)
+}
+
+/// the brake firmware should constrain requested spoof values
+/// upon recieving a brake command message
+fn prop_output_constrained_spoofs(mut brake_command_msg: oscc_brake_command_s) -> TestResult {
+    brake_command_msg.enable = 1u8;
+    unsafe {
+        if brake_command_msg.pedal_command >= 
+            MINIMUM_BRAKE_COMMAND as u16 &&
+            brake_command_msg.pedal_command <=
+            MAXIMUM_BRAKE_COMMAND as u16
+        {
+            return TestResult::discard();
+        }
+
+        g_brake_control_state.enabled = true;
+
+        g_mock_mcp_can_read_msg_buf_id = OSCC_BRAKE_COMMAND_CAN_ID;
+        g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
+        g_mock_mcp_can_read_msg_buf_buf = std::mem::transmute(brake_command_msg);
+
+        check_for_incoming_message();
+
+        TestResult::from_bool(
+            g_brake_control_state.enabled == true)
+    }
+}
+
+#[test]
+fn check_output_constrained_spoofs() {
+    QuickCheck::new()
+        .tests(1000)
+        .gen(StdGen::new(rand::thread_rng(), u16::max_value() as usize))
+        .quickcheck(prop_output_constrained_spoofs as fn(oscc_brake_command_s) -> TestResult)
+}
+
+/// the brake firmware should create only valid CAN frames
+fn prop_send_valid_can_fields(enabled: bool,
+                              operator_override: bool,
+                              dtcs: u8)
+                              -> TestResult {
+    unsafe {
+        g_brake_control_state.operator_override = operator_override;
+        g_brake_control_state.enabled = enabled;
+        g_brake_control_state.dtcs = dtcs;
+
+        publish_brake_report();
+
+        let brake_report_msg = g_mock_mcp_can_send_msg_buf_buf as *mut oscc_brake_report_s;
+
+        TestResult::from_bool((*brake_report_msg).enabled == enabled as u8 &&(*brake_report_msg).operator_override == operator_override as u8 &&
+        (*brake_report_msg).dtcs == dtcs)
+    }
+}
+
+#[test]
+#[ignore]
+fn check_valid_can_frame() {
+    QuickCheck::new()
+        .tests(10)
+        .gen(StdGen::new(rand::thread_rng(), u8::max_value() as usize))
+        .quickcheck(prop_send_valid_can_fields as fn(bool, bool, u8) -> TestResult)
 }
 
 // the brake firmware should be able to correctly and consistently
-// detect operator overrides
+// detect operator overrides, disable on reciept, and send a fault report
 fn prop_check_operator_override(analog_read_spoof: u16) -> TestResult {
     unsafe {
-        static mut filtered_input_1: f32 = 0.0;
-        static mut filtered_input_2: f32 = 0.0;
-        const filter_alpha: f32 = 0.5;
-
         g_brake_control_state.enabled = true;
-        g_mock_arduino_analog_read_return = analog_read_spoof;
+        g_brake_control_state.operator_override = false;
+        g_mock_arduino_analog_read_return = analog_read_spoof as isize;
 
         check_for_operator_override();
 
-        let sensor_1: f32 = raw_adc_to_pressure(analog_read_spoof as i32);
-        let sensor_2: f32 = raw_adc_to_pressure(analog_read_spoof as i32);
-
-        filtered_input_1 = (filter_alpha * sensor_1) + ((1.0 - filter_alpha) * filtered_input_1);
-
-        filtered_input_2 = (filter_alpha * sensor_2) + (1.0 - filter_alpha * filtered_input_2);
-
-        if filtered_input_1.abs() >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS ||
-           filtered_input_2.abs() >= DRIVER_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS {
-            TestResult::from_bool(g_brake_control_state.operator_override == true &&
-                                  g_brake_control_state.enabled == false)
+        if analog_read_spoof >= (BRAKE_OVERRIDE_PEDAL_THRESHOLD_IN_DECIBARS as u16) {
+            TestResult::from_bool(g_brake_control_state.operator_override == true && g_brake_control_state.enabled == false &&
+            g_mock_mcp_can_send_msg_buf_id == OSCC_FAULT_REPORT_CAN_ID)
         } else {
             TestResult::from_bool(g_brake_control_state.operator_override == false)
         }
@@ -269,8 +266,34 @@ fn prop_check_operator_override(analog_read_spoof: u16) -> TestResult {
 }
 
 #[test]
+#[ignore]
 fn check_operator_override() {
     QuickCheck::new()
         .tests(1000)
+        .gen(StdGen::new(rand::thread_rng(), u16::max_value() as usize))
         .quickcheck(prop_check_operator_override as fn(u16) -> TestResult)
+}
+
+/// the brake firmware should set disable itself when it recieves a
+/// fault report from any other module
+fn prop_process_fault_command(enabled: bool, operator_override: bool) -> TestResult {
+    unsafe {
+        g_brake_control_state.enabled = enabled;
+        g_brake_control_state.operator_override = operator_override;
+
+        g_mock_mcp_can_read_msg_buf_id = OSCC_FAULT_REPORT_CAN_ID;
+        g_mock_mcp_can_check_receive_return = CAN_MSGAVAIL as u8;
+
+        check_for_incoming_message();
+
+        TestResult::from_bool(g_brake_control_state.enabled == false)
+    }
+}
+
+#[test]
+#[ignore]
+fn check_process_fault_command() {
+    QuickCheck::new()
+        .tests(1000)
+        .quickcheck(prop_process_fault_command as fn(bool, bool) -> TestResult)
 }
