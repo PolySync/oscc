ba275cc43f41a1ac3e689bafae212a5c6fc71806
Katie Cleary <kcleary@polysync.io>
2017-09-18 14:12:04 -0700

    Add brake PBT
    Prior to this commit, there weren't brake PBT. This commit adds them, along with a CMake flag that enables running either the petrol brake tests or the ev brake tests, since they have different inner functionality. This commit also places common test functionality insid e the unused lib.rs crate, cleaning up some of the directory structure by eliminating the need for an extra module.

--- api/tests/property/src/lib.rs
+++ api/tests/property/src/lib.rs
@@ -9,3 +9,59 @@ include!(concat!(env!("OUT_DIR"), "/oscc_test.rs"));
 extern crate quickcheck;
 extern crate rand;
 extern crate socketcan;
+
+use socketcan::CANSocket;
+use std::time::Duration;
+use std::ffi::CString;
+
+extern "C" {
+    pub static mut can_socket: i32;
+}
+
+pub fn constrain(value: f64, min: f64, max: f64) -> f64 {
+    if value < min {
+        return min;
+    }
+    else if value > max {
+        return max;
+    }
+    value
+}
+
+pub fn open_oscc() {
+    let channel_result = CString::new("vcan0");
+    match channel_result {
+        Ok(channel) => {
+            // initialize oscc socket on vcan0
+            unsafe { oscc_init_can(channel.as_ptr()); }
+        },
+        Err(why) => panic!("{:?}", why)
+    }
+}
+
+pub fn close_oscc() {
+    unsafe { oscc_close(0); }
+}
+
+pub fn skip_enable_frames(socket: &CANSocket) {
+    socket.read_frame();
+    socket.read_frame();
+    socket.read_frame();
+}
+
+pub fn init_socket() -> CANSocket {
+    // initialize our PBT socket on the vcan0
+    let socket_result = CANSocket::open("vcan0");
+    match socket_result {
+        Err(why) => panic!("{:?}", why),
+        Ok(socket) => {
+            socket.set_nonblocking(true);
+            socket.set_read_timeout(Duration::new(1, 0));
+
+            // get rid of initial empty frame
+            socket.read_frame();
+
+            socket
+        }
+    }
+}
--- api/tests/property/tests/brake_tests.rs
+++ api/tests/property/tests/brake_tests.rs
@@ -12,224 +12,167 @@ extern crate socketcan;
 
 extern crate oscc_tests;
 
-mod common;
-
 use quickcheck::{QuickCheck, TestResult, StdGen};
 
-fn get_brake_command_msg_from_buf( buffer: &[u8 ]) -> oscc_steering_command_s {
+fn get_brake_command_msg_from_buf( buffer: &[u8 ]) -> oscc_brake_command_s {
     let data_ptr: *const u8 = buffer.as_ptr();
 
-    let steering_command_ptr: *const oscc_steering_command_s = data_ptr as *const _;
+    let brake_command_ptr: *const oscc_brake_command_s = data_ptr as *const _;
 
-    unsafe { *steering_command_ptr as oscc_steering_command_s }
+    unsafe { *brake_command_ptr as oscc_brake_command_s }
 }
 
-// kia soul petrol brake tests
-fn calculate_petrol_brake_spoofs( torque_command: f64 ) -> ( u16, u16 ) {
-    let scaled_torque = common::constrain(torque_command * MAXIMUM_TORQUE_COMMAND, MINIMUM_TORQUE_COMMAND, MAXIMUM_TORQUE_COMMAND);
+#[cfg(feature = "kia-soul")]
+mod brake_tests {
+    use super::*;
 
-    let mut high_spoof = (TORQUE_SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_SCALE * scaled_torque) + TORQUE_SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_OFFSET;
-    let mut low_spoof = (TORQUE_SPOOF_LOW_SIGNAL_CALIBRATION_CURVE_SCALE * scaled_torque) + TORQUE_SPOOF_LOW_SIGNAL_CALIBRATION_CURVE_OFFSET;
+    /// The API should properly calculate torque spoofs for valid range
+    pub fn prop_valid_brake_spoofs(pedal_command: f64) -> TestResult {
+        let socket = oscc_tests::init_socket();
 
-    high_spoof = common::constrain(high_spoof, STEERING_SPOOF_HIGH_SIGNAL_VOLTAGE_MIN, STEERING_SPOOF_HIGH_SIGNAL_VOLTAGE_MAX);
+        unsafe { oscc_enable() };
 
-    low_spoof = common::constrain(low_spoof, STEERING_SPOOF_LOW_SIGNAL_VOLTAGE_MIN, STEERING_SPOOF_LOW_SIGNAL_VOLTAGE_MAX);
+        oscc_tests::skip_enable_frames(&socket);
 
-    ((high_spoof  * STEPS_PER_VOLT) as u16, (low_spoof * STEPS_PER_VOLT) as u16)
+        // send some command
+        unsafe { oscc_publish_brake_position(pedal_command.abs()); }
+
+        // read from can frame
+        let frame_result = socket.read_frame();
+        match frame_result {
+            Err(why) => TestResult::discard(),
+            Ok(frame) => {
+                let brake_command_msg = get_brake_command_msg_from_buf( frame.data() );
+
+                let scaled_pedal_command = oscc_tests::constrain(pedal_command.abs() * (MAXIMUM_BRAKE_COMMAND as f64), MINIMUM_BRAKE_COMMAND as f64, MAXIMUM_BRAKE_COMMAND as f64);
+
+                TestResult::from_bool(brake_command_msg.pedal_command == scaled_pedal_command as u16)
+            }
+        }
+    }
+
+    /// For any valid steering input, the API should never send a spoof value 
+    /// outside of the valid range
+    pub fn prop_constrain_brake_spoofs(pedal_command: f64) -> TestResult {
+        let socket = oscc_tests::init_socket();
+
+        unsafe { oscc_enable() };
+
+        oscc_tests::skip_enable_frames(&socket);
+
+        // send some command
+        unsafe { oscc_publish_brake_position(pedal_command.abs()); }
+
+        // read from can frame
+        let frame_result = socket.read_frame();
+        match frame_result {
+            Err(why) => TestResult::discard(),
+            Ok(frame) => {
+                let brake_command_msg = get_brake_command_msg_from_buf( frame.data() );
+
+                TestResult::from_bool( 
+                    (brake_command_msg.pedal_command >= MINIMUM_BRAKE_COMMAND as u16) && 
+                    (brake_command_msg.pedal_command <= MAXIMUM_BRAKE_COMMAND as u16))
+            }
+        }
+    }
 }
 
-/// The API should properly calculate torque spoofs for valid range
-fn prop_valid_petrol_brake_spoofs(steering_torque: f64) -> TestResult {
-    let socket = common::init_socket();
+#[cfg(feature = "kia-soul-ev")]
+mod brake_tests {
+    use super::*;
 
-    unsafe { oscc_enable() };
+    /// The API should properly calculate torque spoofs for valid range
+    fn calculate_ev_brake_spoofs( brake_command: f64 ) -> ( u16, u16 ) {
+        let high_spoof = brake_command * (BRAKE_SPOOF_HIGH_SIGNAL_VOLTAGE_MAX - BRAKE_SPOOF_HIGH_SIGNAL_VOLTAGE_MIN) + BRAKE_SPOOF_HIGH_SIGNAL_VOLTAGE_MIN;
+        let low_spoof = brake_command * (BRAKE_SPOOF_LOW_SIGNAL_VOLTAGE_MAX - BRAKE_SPOOF_LOW_SIGNAL_VOLTAGE_MIN) + BRAKE_SPOOF_LOW_SIGNAL_VOLTAGE_MIN;
+        ((high_spoof  * STEPS_PER_VOLT) as u16, (low_spoof * STEPS_PER_VOLT) as u16)
+    }
 
-    common::skip_enable_frames(&socket);
+    /// The API should properly calculate torque spoofs for valid range
+    pub fn prop_valid_brake_spoofs(pedal_command: f64) -> TestResult {
+        let socket = oscc_tests::init_socket();
 
-    // send some command
-    unsafe { oscc_publish_steering_torque(steering_torque); }
+        unsafe { oscc_enable() };
 
-    // read from can frame
-    let frame_result = socket.read_frame();
-    match frame_result {
-        Err(why) => TestResult::discard(),
-        Ok(frame) => {
-            let torque_command_msg = get_ev_brake_command_msg_from_buf( frame.data() );
+        oscc_tests::skip_enable_frames(&socket);
 
-            let actual_spoofs = (torque_command_msg.spoof_value_high, torque_command_msg.spoof_value_low);
+        // send some command
+        unsafe { oscc_publish_brake_position(pedal_command.abs()); }
 
-            let expected_spoofs = calculate_ev_brake_spoofs(steering_torque);
+        // read from can frame
+        let frame_result = socket.read_frame();
+        match frame_result {
+            Err(why) => TestResult::discard(),
+            Ok(frame) => {
+                let brake_command_msg = get_brake_command_msg_from_buf( frame.data() );
 
-            TestResult::from_bool(actual_spoofs == expected_spoofs)
+                let actual_spoofs = (brake_command_msg.spoof_value_high, brake_command_msg.spoof_value_low);
+
+                let expected_spoofs = calculate_ev_brake_spoofs(pedal_command.abs());
+
+                TestResult::from_bool(actual_spoofs == expected_spoofs)
+            }
+        }
+    }
+
+    /// For any valid steering input, the API should never send a spoof value 
+    /// outside of the valid range
+    pub fn prop_constrain_brake_spoofs(pedal_command: f64) -> TestResult {
+        let socket = oscc_tests::init_socket();
+
+        unsafe { oscc_enable() };
+
+        oscc_tests::skip_enable_frames(&socket);
+
+        // send some command
+        unsafe { oscc_publish_brake_position(pedal_command.abs()); }
+
+        // read from can frame
+        let frame_result = socket.read_frame();
+        match frame_result {
+            Err(why) => TestResult::discard(),
+            Ok(frame) => {
+                let brake_command_msg = get_brake_command_msg_from_buf( frame.data() );
+
+                let spoof_high = brake_command_msg.spoof_value_high as u32;
+                let spoof_low = brake_command_msg.spoof_value_low as u32;
+
+                TestResult::from_bool( 
+                    (spoof_high <= BRAKE_SPOOF_HIGH_SIGNAL_RANGE_MAX) && 
+                    (spoof_high >= BRAKE_SPOOF_HIGH_SIGNAL_RANGE_MIN) && (spoof_low <= BRAKE_SPOOF_LOW_SIGNAL_RANGE_MAX) && (spoof_low >= BRAKE_SPOOF_LOW_SIGNAL_RANGE_MIN))
+            }
         }
     }
 }
 
 #[test]
-fn check_valid_petrol_brake_spoofs() {
-    common::open_oscc();
+fn check_valid_brake_spoofs() {
+    oscc_tests::open_oscc();
 
     let ret = QuickCheck::new()
         .tests(1000)
         .gen(StdGen::new(rand::thread_rng(), 1 as usize))
-        .quickcheck(prop_valid_petrol_brake_spoofs as fn(f64) -> TestResult);
+        .quickcheck(brake_tests::prop_valid_brake_spoofs as fn(f64) -> TestResult);
 
-    common::close_oscc();
+    oscc_tests::close_oscc();
 
     ret
 }
 
-/// For any valid steering input, the API should never send a spoof value 
-/// outside of the valid range
-fn prop_constrain_petrol_brake_spoofs(steering_command: f64) -> TestResult {
-    let socket = common::init_socket();
-
-    unsafe { oscc_enable() };
-
-    common::skip_enable_frames(&socket);
-
-    // send some command
-    unsafe { oscc_publish_steering_torque(steering_command); }
-
-    // read from can frame
-    let frame_result = socket.read_frame();
-    match frame_result {
-        Err(why) => TestResult::discard(),
-        Ok(frame) => {
-            let steering_command_msg = get_ev_brake_command_msg_from_buf( frame.data() );
-
-            let spoof_high = steering_command_msg.spoof_value_high as u32;
-            let spoof_low = steering_command_msg.spoof_value_low as u32;
-
-            // fails on 1 w high-min of 737 -- need to constrain from API possibly
-
-            TestResult::from_bool( 
-                (spoof_high <= STEERING_SPOOF_HIGH_SIGNAL_RANGE_MAX) && 
-                (spoof_high >= STEERING_SPOOF_HIGH_SIGNAL_RANGE_MIN) && (spoof_low <= STEERING_SPOOF_LOW_SIGNAL_RANGE_MAX) && (spoof_low >= STEERING_SPOOF_LOW_SIGNAL_RANGE_MIN))
-        }
-    }
-}
-
 #[test]
-#[ignore]
-fn check_constrain_petrol_brake_spoofs() {
-    common::open_oscc();
+fn check_constrain_brake_spoofs() {
+    oscc_tests::open_oscc();
 
     let ret = QuickCheck::new()
         .tests(1000)
         .gen(StdGen::new(rand::thread_rng(), std::f64::MAX as usize))
-        .quickcheck(prop_constrain_petrol_brake_spoofs as fn(f64) -> TestResult);
+        .quickcheck(brake_tests::prop_constrain_brake_spoofs as fn(f64) -> TestResult);
 
-    common::close_oscc();
-
-    ret
-}
-
-// kia soul ev brake tests
-fn calculate_ev_brake_spoofs( torque_command: f64 ) -> ( u16, u16 ) {
-    let scaled_torque = common::constrain(torque_command * MAXIMUM_TORQUE_COMMAND, MINIMUM_TORQUE_COMMAND, MAXIMUM_TORQUE_COMMAND);
-
-    let mut high_spoof = (TORQUE_SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_SCALE * scaled_torque) + TORQUE_SPOOF_HIGH_SIGNAL_CALIBRATION_CURVE_OFFSET;
-    let mut low_spoof = (TORQUE_SPOOF_LOW_SIGNAL_CALIBRATION_CURVE_SCALE * scaled_torque) + TORQUE_SPOOF_LOW_SIGNAL_CALIBRATION_CURVE_OFFSET;
-
-    high_spoof = common::constrain(high_spoof, STEERING_SPOOF_HIGH_SIGNAL_VOLTAGE_MIN, STEERING_SPOOF_HIGH_SIGNAL_VOLTAGE_MAX);
-
-    low_spoof = common::constrain(low_spoof, STEERING_SPOOF_LOW_SIGNAL_VOLTAGE_MIN, STEERING_SPOOF_LOW_SIGNAL_VOLTAGE_MAX);
-
-    ((high_spoof  * STEPS_PER_VOLT) as u16, (low_spoof * STEPS_PER_VOLT) as u16)
-}
-
-fn get_ev_brake_command_msg_from_buf( buffer: &[u8 ]) -> oscc_steering_command_s {
-    let data_ptr: *const u8 = buffer.as_ptr();
-
-    let steering_command_ptr: *const oscc_steering_command_s = data_ptr as *const _;
-
-    unsafe { *steering_command_ptr as oscc_steering_command_s }
-}
-
-/// The API should properly calculate torque spoofs for valid range
-fn prop_valid_ev_brake_spoofs(steering_torque: f64) -> TestResult {
-    let socket = common::init_socket();
-
-    unsafe { oscc_enable() };
-
-    common::skip_enable_frames(&socket);
-
-    // send some command
-    unsafe { oscc_publish_steering_torque(steering_torque); }
-
-    // read from can frame
-    let frame_result = socket.read_frame();
-    match frame_result {
-        Err(why) => TestResult::discard(),
-        Ok(frame) => {
-            let torque_command_msg = get_ev_brake_command_msg_from_buf( frame.data() );
-
-            let actual_spoofs = (torque_command_msg.spoof_value_high, torque_command_msg.spoof_value_low);
-
-            let expected_spoofs = calculate_ev_brake_spoofs(steering_torque);
-
-            TestResult::from_bool(actual_spoofs == expected_spoofs)
-        }
-    }
-}
-
-#[test]
-fn check_valid_ev_brake_spoofs() {
-    common::open_oscc();
-
-    let ret = QuickCheck::new()
-        .tests(1000)
-        .gen(StdGen::new(rand::thread_rng(), 1 as usize))
-        .quickcheck(prop_valid_ev_brake_spoofs as fn(f64) -> TestResult);
-
-    common::close_oscc();
+    oscc_tests::close_oscc();
 
     ret
 }
 
-/// For any valid steering input, the API should never send a spoof value 
-/// outside of the valid range
-fn prop_constrain_ev_brake_spoofs(steering_command: f64) -> TestResult {
-    let socket = common::init_socket();
 
-    unsafe { oscc_enable() };
 
-    common::skip_enable_frames(&socket);
-
-    // send some command
-    unsafe { oscc_publish_steering_torque(steering_command); }
-
-    // read from can frame
-    let frame_result = socket.read_frame();
-    match frame_result {
-        Err(why) => TestResult::discard(),
-        Ok(frame) => {
-            let steering_command_msg = get_ev_brake_command_msg_from_buf( frame.data() );
-
-            let spoof_high = steering_command_msg.spoof_value_high as u32;
-            let spoof_low = steering_command_msg.spoof_value_low as u32;
-
-            // fails on 1 w high-min of 737 -- need to constrain from API possibly
-
-            TestResult::from_bool( 
-                (spoof_high <= STEERING_SPOOF_HIGH_SIGNAL_RANGE_MAX) && 
-                (spoof_high >= STEERING_SPOOF_HIGH_SIGNAL_RANGE_MIN) && (spoof_low <= STEERING_SPOOF_LOW_SIGNAL_RANGE_MAX) && (spoof_low >= STEERING_SPOOF_LOW_SIGNAL_RANGE_MIN))
-        }
-    }
-}
-
-#[test]
-#[ignore]
-fn check_constrain_ev_brake_spoofs() {
-    common::open_oscc();
-
-    let ret = QuickCheck::new()
-        .tests(1000)
-        .gen(StdGen::new(rand::thread_rng(), std::f64::MAX as usize))
-        .quickcheck(prop_constrain_ev_brake_spoofs as fn(f64) -> TestResult);
-
-    common::close_oscc();
-
-    ret
-}
