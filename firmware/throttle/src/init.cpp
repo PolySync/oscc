a11b9cfaaad3a84b59cb1a77d3d96a2b433b471b
Robert Brown <rbrown@polysync.io>
2018-01-15 12:49:10 -0800

    Add Secondary Filtering
    Prior to this commit the second set of filters were ignored which would not
    utilize the second buffer when the first one overflows. This commit adds a
    filter to the second buffer allowing for only the CAN ID of disable should a
    buffer overflow occur so that disable is still available.

--- firmware/throttle/src/init.cpp
+++ firmware/throttle/src/init.cpp
@@ -52,8 +52,11 @@ void init_communication_interfaces( void )
     init_can( g_control_can );
 
     // Filter CAN messages - accept if (CAN_ID & mask) == (filter & mask)
+    // Set buffer 0 to filter only brake module and global messages
     g_control_can.init_Mask( 0, 0, 0x7F0 ); // Filter for 0x0N0 to 0x0NF
-    g_control_can.init_Mask( 1, 0, 0x7FF ); // Don't use second filter
     g_control_can.init_Filt( 0, 0, OSCC_THROTTLE_CAN_ID_INDEX );
     g_control_can.init_Filt( 1, 0, OSCC_GLOBAL_CAN_ID_INDEX );
+    // Accept only CAN Disable when buffer overflow occurs in buffer 0
+    g_control_can.init_Mask( 1, 0, 0x7FF ); // Filter for one CAN ID
+    g_control_can.init_Filt( 2, 1, OSCC_THROTTLE_DISABLE_CAN_ID );
 }
