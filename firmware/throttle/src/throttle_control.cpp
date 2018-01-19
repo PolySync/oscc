fbdddf03e69b5d0b9367eac3fc45287ae492a323
Robert Brown <rbrown@polysync.io>
2018-01-19 15:46:35 -0800

    Remove throttle timeout and lower threshold
    Prior to this commit the niro would fault when not desireable this commit
    adds delay to ensure override is being requested.

--- firmware/throttle/src/throttle_control.cpp
+++ firmware/throttle/src/throttle_control.cpp
@@ -17,6 +17,8 @@
 #include "vehicles.h"
 #include "status.h"
 
+static unsigned long TIME_LAST_SEEN = 0;
+const unsigned long DELAY = 200; //ms of delay to ensure override still requested
 
 /*
  * @brief Number of consecutive faults that can occur when reading the
@@ -47,23 +49,34 @@ void check_for_operator_override( void )
 
         if ( accelerator_position_average >= ACCELERATOR_OVERRIDE_THRESHOLD )
         {
-            disable_control( );
+            unsigned long current_time = millis();
 
-            status_setGreenLed(0);
-            status_setRedLed(1);
+            if ( TIME_LAST_SEEN == 0 )
+            {
+                TIME_LAST_SEEN = millis();
+            }
+            else if ( current_time - TIME_LAST_SEEN > DELAY )
+            {
+                disable_control( );
 
-            DTC_SET(
-                g_throttle_control_state.dtcs,
-                OSCC_THROTTLE_DTC_OPERATOR_OVERRIDE );
+                status_setGreenLed(0);
+                status_setRedLed(1);
 
-            publish_fault_report( );
+                DTC_SET(
+                    g_throttle_control_state.dtcs,
+                    OSCC_THROTTLE_DTC_OPERATOR_OVERRIDE );
 
-            g_throttle_control_state.operator_override = true;
+                publish_fault_report( );
 
-            DEBUG_PRINTLN( "Operator override" );
+                g_throttle_control_state.operator_override = true;
+
+                DEBUG_PRINTLN( "Operator override" );
+            }
         }
         else
         {
+            TIME_LAST_SEEN = 0; //Start over no deviation
+
             DTC_CLEAR(
                 g_throttle_control_state.dtcs,
                 OSCC_THROTTLE_DTC_OPERATOR_OVERRIDE );
