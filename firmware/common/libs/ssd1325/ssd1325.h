63e158bd5a661eb315e47f8b2ce99748f9ba188a
Trey German <trey@polymorphiclabs.com>
2018-01-24 13:02:30 -0600

    Fixed typo in header file

--- firmware/common/libs/ssd1325/ssd1325.h
+++ firmware/common/libs/ssd1325/ssd1325.h
@@ -82,7 +82,7 @@ class SSD1325 : public GFX {
         void enableRedLed(void);
         void enableYellowLed(void);
         void enableGreenLed(void);
-        void disableLeds(voids);
+        void disableLeds(void);
 
     private:
         int8_t cs;
